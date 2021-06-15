c4b7c27aef2ed1dc456dca74c5619cabe30da6fe
Ana Lucia Morales <analuciamrls@gmail.com>
2021-06-15 14:24:18 -0300

    Primer commit

---
+++ TP6/TP6/Calculadora.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP6
+{
+    public class Calculadora
+    {
+        private float numero1;
+
+        public float Numero1 { get => numero1; set => numero1 = value; }        
+
+        private float numero2;
+
+        public float Numero2 { get => numero2; set => numero2 = value; }
+
+        public float Suma()
+        {
+            return numero1 + numero2;
+        }
+
+        public float Resta()
+        {
+            return numero1 - numero2;
+        }
+
+        public float Multiplicacion()
+        {
+            return numero1 * numero2;
+        }
+
+        /*public float Division()
+        {
+            if (numero2 > 0)
+            {
+                return numero1 / numero2;
+            }
+            else
+            {
+                return 
+            }
+            
+        }*/
+
+    }
+}
